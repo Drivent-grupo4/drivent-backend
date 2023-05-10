4387b412abbc86558936dddf824feee5cff62181
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-05-10 11:22:11 -0300

    chore: ticket type seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,6 +16,29 @@ async function main() {
     });
   }
 
+  await prisma.ticketType.createMany({
+    data: [
+      {
+        name: "Online",
+        price: 100,
+        isRemote: true,
+        includesHotel: false,
+      },
+      {
+        name: "Presencial Com Hotel",
+        price: 600,
+        isRemote: false,
+        includesHotel: true,
+      },
+      {
+        name: "Presencial Sem Hotel",
+        price: 250,
+        isRemote: false,
+        includesHotel: false,
+      },
+    ]
+  })
+
   console.log({ event });
 }
 
