a324366a9d0d8aefeac4234c1a807dbb51aa87db
Danilo Leão <olin_ad@hotmail.com>
2023-05-18 08:39:42 -0300

    feat: switched events db to redis on cache

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,20 @@
-import { prisma } from '@/config';
+import { Event } from '@prisma/client';
+import { prisma, redis } from '@/config';
 
-async function findFirst() {
-  return prisma.event.findFirst();
+async function findFirst(): Promise<Event> {
+  const cacheKey = 'event';
+  const expiration = 3600 * 24;
+  const cachedEvent = await redis.get(cacheKey);
+  // redis.del(cacheKey);
+
+  if (cachedEvent !== 'null') {
+    console.log('cached');
+    return JSON.parse(cachedEvent);
+  }
+  const event = await prisma.event.findFirst();
+
+  redis.setEx(cacheKey, expiration, JSON.stringify(event));
+  return event;
 }
 
 const eventRepository = {
